b96bb26f78f95d6d35996e0d599ca890597b9283
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-06-16 21:57:28 +0300

    Updates

---
+++ src_pages_not-found-page_ui_not-found-page_tsx.3a53f6f9717146ccb0af.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunke_commerce"] = self["webpackChunke_commerce"] || []).push([["src_pages_not-found-page_ui_not-found-page_tsx"],{
+
+/***/ "./src/pages/not-found-page/ui/not-found-page.tsx":
+/*!********************************************************!*\
+  !*** ./src/pages/not-found-page/ui/not-found-page.tsx ***!
+  \********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var NotFoundPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "NotFoundPage" }, void 0));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NotFoundPage);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX25vdC1mb3VuZC1wYWdlX3VpX25vdC1mb3VuZC1wYWdlX3RzeC4zYTUzZjZmOTcxNzE0NmNjYjBhZi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFnRDtBQUNoRDtBQUNBLFlBQVksc0RBQUksVUFBVSwwQkFBMEI7QUFDcEQ7QUFDQSxpRUFBZSxZQUFZLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9lLWNvbW1lcmNlLy4vc3JjL3BhZ2VzL25vdC1mb3VuZC1wYWdlL3VpL25vdC1mb3VuZC1wYWdlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBqc3ggYXMgX2pzeCB9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xyXG52YXIgTm90Rm91bmRQYWdlID0gZnVuY3Rpb24gKCkge1xyXG4gICAgcmV0dXJuIChfanN4KFwiZGl2XCIsIHsgY2hpbGRyZW46IFwiTm90Rm91bmRQYWdlXCIgfSwgdm9pZCAwKSk7XHJcbn07XHJcbmV4cG9ydCBkZWZhdWx0IE5vdEZvdW5kUGFnZTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
