a4935a1c1d6b7394e56af09ff66f1edf68bc42ed
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-06-16 23:56:06 +0300

    feat: add HotModuleReplacementPlugin

--- src/shared/config/route-config/route-config.tsx
+++ src/shared/config/route-config/route-config.tsx
@@ -12,10 +12,10 @@ export enum AppRoutes {
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-	[AppRoutes.MAIN]: 'e-commerce/',
-	[AppRoutes.CART]: 'e-commerce/cart',
-	[AppRoutes.ORDER]: 'e-commerce/order',
-	[AppRoutes.NOT_FOUND]: 'e-commerce/*',
+	[AppRoutes.MAIN]: '/e-commerce/',
+	[AppRoutes.CART]: '/e-commerce/cart',
+	[AppRoutes.ORDER]: '/e-commerce/order',
+	[AppRoutes.NOT_FOUND]: '/e-commerce/*',
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
